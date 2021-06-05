0ae7c8a7eec1eca5f838110e963bd37a1db771a8
Anathan <anathan.t.pereira@gmail.com>
2021-06-05 19:23:51 -0300

    Auto stash before merge of "read_save_a" and "origin/read_save_a"

---
+++ Projection.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def perspective(right, top, near, far):
+    A = near/right
+    B = near/top
+    C = -(far+near)/(far-near)
+    D = -2*far*near/(far-near)
+
+    return np.array((
+        ( A, 0, 0, 0),
+        ( 0, B, 0, 0),
+        ( 0, 0, C, -1)
+        ( 0, 0, D, 0)
+    ))
+    
